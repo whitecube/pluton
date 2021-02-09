3a9eebb790d35f2a4f66c39c83b2b51063cc7d09
Adrien Leloup <adrien@whitecube.be>
2021-02-09 12:17:35 +0100

    Moved instances into property, added clear method to remove instances.

--- pluton.js
+++ pluton.js
@@ -2,7 +2,12 @@ export default class Pluton {
 
     constructor() {
         this.classes = this.importAll();
+        this.instances = {};
         this.setup();
+    }
+
+    clear() {
+        this.instances = {};
     }
 
     setup(root) {
@@ -14,15 +19,15 @@ export default class Pluton {
     setupComponent(className, component, root) {
         if (!component.selector) return;
         [].forEach.call((root||document).querySelectorAll(component.selector), (el) => {
-            if (!this[className]) this[className] = [];
-            this[className].push(new component(el));
+            if (!this.instances[className]) this.instances[className] = [];
+            this.instances[className].push(new component(el));
         });
     }
 
     call(className, fn, parameters) {
-        if (!this[className]) return;
-        for (var i = this[className].length - 1; i >= 0; i--) {
-            this[className][i][fn](parameters);
+        if (!this.instances[className]) return;
+        for (var i = this.instances[className].length - 1; i >= 0; i--) {
+            this.instances[className][i][fn](parameters);
         }
     }
 
