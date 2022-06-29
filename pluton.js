538df1b5e4f0cdb3937eb123187090d270173bc2
Adrien Leloup <adrien@whitecube.be>
2022-06-29 11:22:15 +0200

    WIP Vite support

--- pluton.js
+++ pluton.js
@@ -1,13 +1,35 @@
 export default class Pluton {
 
     constructor() {
-        this.classes = this.importAll();
         this.instances = {};
-        this.setup();
+
+        this.loadModules().then(classes => {
+            this.classes = classes;
+            this.setup();
+        });
     }
 
-    clear() {
-        this.instances = {};
+    async loadModules() {
+        const modules = import.meta.glob('./parts/*.js');
+        const promises = [];
+        const paths = Object.keys(modules);
+
+        for (let index = 0; index < paths.length; index++) {
+            promises.push(modules[paths[index]]());
+        }
+
+        const loaded = await Promise.all(promises);
+
+        return new Promise(resolve => {
+            const classes = {};
+
+            loaded.forEach(mod => {
+                const code = mod.default;
+                classes[code.selector] = code;
+            });
+
+            resolve(classes);
+        });
     }
 
     setup(root) {
@@ -17,27 +39,16 @@ export default class Pluton {
     }
 
     setupComponent(className, component, root) {
-        if (!component.selector) return;
+        if (! component.selector) {
+            return;
+        }
+
         [].forEach.call((root||document).querySelectorAll(component.selector), (el) => {
-            if (!this.instances[className]) this.instances[className] = [];
+            if (! this.instances[className]) {
+                this.instances[className] = [];
+            }
+
             this.instances[className].push(new component(el));
         });
-    }
-
-    call(className, fn, parameters) {
-        if (!this.instances[className]) return;
-        for (var i = this.instances[className].length - 1; i >= 0; i--) {
-            this.instances[className][i][fn](parameters);
-        }
-    }
-
-    importAll() {
-        var context = require.context(PLUTON_PATH, true, /\.js$/);
-        var obj = {};
-        context.keys().forEach(key => {
-            let code = context(key);
-            obj[code.default.selector] = code.default;
-        });
-        return obj;
     }
 }
