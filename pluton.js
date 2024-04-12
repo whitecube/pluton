2d4d9f15162a840209f1299cdd140afc21aa48b5
théoo <90846584+theokbokki@users.noreply.github.com>
2024-04-12 15:52:31 +0200

    re-add call function

--- pluton.js
+++ pluton.js
@@ -51,4 +51,11 @@ export default class Pluton {
             this.instances[className].push(new component(el));
         });
     }
+
+    call(className, fn, parameters) {
+        if (!this.instances[className]) return;
+        for (var i = this.instances[className].length - 1; i >= 0; i--) {
+            this.instances[className][i][fn](parameters);
+        }
+    }
 }
